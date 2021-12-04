d2a3860ea14b3c1fa211bc053ab461e86f2da04b
von9711 <62811612+von9711@users.noreply.github.com>
2021-12-05 00:28:44 +0530

    add appi to fetch data from backend

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import articles from "./article-content";
 import ArticlesList from "../components/ArticlesList";
 import NotFoundPage from "./NotFoundPage";
+import CommentsList from "../components/CommentsList";
+import UpvotesSection from "../components/UpvotesSection";
 
 const ArticlePage = () => {
   const { name } = useParams();
   const article = articles.find((article) => article.name === name);
   const otherArticles = articles.filter((article) => article.name !== name);
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`http://localhost:8000/api/articles/${name}`);
+      const body = await result.json();
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
 
   return !article ? (
     <NotFoundPage />
   ) : (
     <>
-      <h1>Article Page: {name}</h1>
       <h3>{article.title}</h3>
+      <UpvotesSection
+        articleName={name}
+        upvotes={articleInfo.upvotes}
+        setArticleInfo={setArticleInfo}
+      />
       {article.content.map((paragraph, index) => (
         <p key={index}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
       <ArticlesList articles={otherArticles} />
     </>
   );
